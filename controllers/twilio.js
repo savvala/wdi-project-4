4ef82726281fcaaaee7c18a8ced8ba004d954d05
savvala <savva.l@hotmail.com>
2017-11-02 17:34:28 +0000

    added node env to twilio redirect uri

--- controllers/twilio.js
+++ controllers/twilio.js
@@ -2,13 +2,14 @@ const twilio = require('twilio');
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID ;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
+const { twilioRedirectURI } = require('../config/environment');
 
 const client = new twilio(accountSid, authToken);
 
 function sendMessage(req, res, next) {
 
   client.messages.create({
-    body: `${req.currentUser.cards[0].fullName} wants to trade cards! Click here: http://localhost:8000/cards/${req.currentUser.cards[0].id} 💵`,
+    body: `${req.currentUser.cards[0].fullName} wants to trade cards! Click here: ${twilioRedirectURI}${req.currentUser.cards[0].id} 💵`,
     to: req.body.to,  // Text this number
     from: '+447481339063' // From a valid Twilio number
   })

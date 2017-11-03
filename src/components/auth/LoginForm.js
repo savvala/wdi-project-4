94de0ae717aca705bfb43504040f2d2d0571275d
savvala <savva.l@hotmail.com>
2017-11-03 10:08:38 +0000

    Adding auth test

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -2,30 +2,34 @@ import React from 'react';
 
 const LoginForm = ({ handleChange, handleSubmit, credentials, error }) => {
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="col-md-6 offset-md-3">
-        <input
-          type="text"
-          name="email"
-          placeholder="Email"
-          onChange={handleChange}
-          value={credentials.email}
-          className="form-control"
-        />
-      </div>
-      <div className="col-md-6 offset-md-3">
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          onChange={handleChange}
-          value={credentials.password}
-          className="form-control"
-        />
-        {error && <small className="has-error">{error}</small>}
-      </div>
-      <button className="btn btn-primary">Login</button>
-    </form>
+    <div className="row">
+      <form onSubmit={handleSubmit} className="col-md-6 offset-md-3">
+        <div className="non-card">
+          <div>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              onChange={handleChange}
+              value={credentials.email}
+              className="form-control"
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={handleChange}
+              value={credentials.password}
+              className="form-control"
+            />
+            {error && <small className="has-error">{error}</small>}
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
+    </div>
   );
 };
 

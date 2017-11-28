68e37df8c2126450334b45c3a64bcc69b1c90235
savvala <savva.l@hotmail.com>
2017-11-28 14:25:27 +0000

    changed redirect uri

--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,6 @@ const env = process.env.NODE_ENV || 'development';
 const port  = process.env.PORT || 4000;
 const dbURI = process.env.MONGODB_URI || `mongodb://localhost/project-4-${env}`;
 const secret = process.env.AUTH_SECRET || 'hYgs^=?>@qrTfxLp';
-const twilioRedirectURI = process.env.NODE_ENV === 'production' ? 'https://thawing-plains-91311.herokuapp.com/cards/' : 'http://localhost:8000/cards/';
+const twilioRedirectURI = process.env.NODE_ENV === 'production' ? 'https://trade-contact.herokuapp.com/cards/' : 'http://localhost:8000/cards/';
 
 module.exports = { port, dbURI, secret, env, twilioRedirectURI };
